0fa0d80575e974712cba0a26ad1f688212d82f3b
Linya Hu <lh62594@yahoo.com>
2019-01-31 16:33:30 -0500

    edit

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,38 +6,74 @@ class FiguresController < ApplicationController
   end
 
   get '/figures/new' do
-
     erb :'/figures/new'
   end
 
   post '/figures' do
-    binding.pry
-    @figure = Figure.create(params[:name])
-    if params[:title_ids] == nil && params[:landmark_ids] == nil
-      @title = Title.create(params[:title][:name])
+    @figure = Figure.create(name: params[:name])
+
+    if params[:title_ids] == nil
+      @title = Title.create(name: params[:title][:name])
       FigureTitle.create(figure_id: @figure.id, title_id: @title.id)
     else
       params[:title_ids].each do |title_id|
       FigureTitle.create(figure_id: @figure.id, title_id: title_id.to_i)
+      end
     end
+
     if params[:landmark_ids] == nil
-      @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed], figure_id: @figure.id)
+        @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed], figure_id: @figure.id)
     else
-      @landmark = Landmark.find(params[:landmark_ids])
-      @landmark.update(figure_id = @figure.id)
-    end 
-  end
+      params[:landmark_ids].each do |landmark_id|
+        @landmark = Landmark.find(landmark_id.to_i)
+        @landmark.update(figure_id: @figure.id)
+      end
+    end
+
     redirect :"/figures/#{@figure.id}"
   end
 
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
-    erb :'/figures/show'
+    erb :"/figures/show"
   end
 
 
   get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
 
+  patch '/figures/:id' do
+    # binding.pry
+    @figure = Figure.find(params[:id])
+
+    if params[:title_ids] == nil
+      @title = Title.create(name: params[:title][:name])
+      FigureTitle.create(figure_id: @figure.id, title_id: @title.id)
+    else
+      FigureTitle.select(figure_id: @figure.id).each do |figure_title|
+        figure_title.destroy
+      end
+      params[:title_ids].each do |title_id|
+      FigureTitle.create(figure_id: @figure.id, title_id: title_id.to_i)
+      end
+    end
+
+    Landmark.select(figure_id: @figure.id).each do |landmark|
+      landmark.figure_id.delete
+    end
+
+    if params[:landmark_ids] == nil
+        @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed], figure_id: @figure.id)
+    else
+      params[:landmark_ids].each do |landmark_id|
+        @landmark = Landmark.find(landmark_id.to_i)
+        @landmark.update(figure_id: @figure.id)
+      end
+    end
+
+    redirect "/figures/#{@figure.id}"
   end
 
 end
