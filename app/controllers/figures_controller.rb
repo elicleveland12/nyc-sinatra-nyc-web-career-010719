046ace35754faecd7ceec620005f7ed9041c033d
Linya Hu <lh62594@yahoo.com>
2019-01-31 10:42:48 -0500

    created new form for figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,17 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures/new' do
+
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+
+  end
+
+
+  get '/figures/:id/edit' do
+    
+  end
+
 end
